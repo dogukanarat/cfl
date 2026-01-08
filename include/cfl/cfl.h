6c816e2fff7f1f328837ebcded4eec195dddf351
dogukanarat <aratdogukan@gmail.com>
2026-01-08 23:39:10 +0300

    Add comments

--- include/cfl/cfl.h
+++ include/cfl/cfl.h
@@ -31,33 +31,35 @@ extern "C"
 
 // clang-format off
 
+/// @brief CFL Protocol Definitions
 #define CFL_SYNC_WORD           (0x0A50)
 #define CFL_VERSION             (1)
 #define CFL_HEADER_SIZE         (sizeof(cfl_message_t))
 
+/// @brief CFL Message Flags
 #define CFL_F_RQST              (1u << 0)
 #define CFL_F_RPLY              (1u << 1)
 #define CFL_F_PUSH              (1u << 2)
 #define CFL_F_ACK               (1u << 3)
 #define CFL_F_NACK              (1u << 4)
-#define CFL_F_ENCRYPTED         (1u << 5)
-#define CFL_F_COMPRESSED        (1u << 6)
-#define CFL_F_RESERVED          (1u << 7)
+#define CFL_F_RESERVED0         (1u << 5)
+#define CFL_F_RESERVED1         (1u << 6)
+#define CFL_F_RESERVED2         (1u << 7)
 
 // clang-format on
 
 /* Types */
 
+/// @brief Control Freak Lite message structure
+/// @note All multi-byte fields are in big-endian format
 typedef struct cfl_message_s
 {
-    /* Header */
     uint16_t sync;   ///< Sync word
     uint8_t version; ///< Protocol version
     uint8_t flags;   ///< Message flags
     uint16_t id;     ///< Message ID
     uint16_t seq;    ///< Sequence number
     uint16_t length; ///< Length of payload in bytes
-    /* Payload */
     union
     {
         uint8_t data[0];
